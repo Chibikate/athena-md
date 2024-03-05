c96429987a0c7d04e9bbe32901285093c1d4d3f0
Chibikate <queenie.cabanilla@gmail.com>
2024-03-05 23:52:25 +0800

    Proceed to the Next Lesson

--- app/certificate/adddicom/page.js
+++ app/certificate/adddicom/page.js
@@ -49,7 +49,7 @@ function CertificatePage() {
       <div>
         <Link href="/course/3D%20slicer%20-%20Navigating%20DICOM%20display">
           <button className="bg-[#1c1648] text-white px-4 py-2 cursor-pointer rounded-md hover:bg-indigo-600 mt-4 inline-block">
-            Click here to proceed to the next Lesson
+          Proceed to the Next Lesson
           </button>
         </Link>
       </div>
--- app/certificate/interface/page.js
+++ app/certificate/interface/page.js
@@ -48,7 +48,7 @@ function CertificatePage() {
       <div>
         <Link href="/course/3D%20slicer%20-%20Adding%20DICOM%20datasets">
           <button className="bg-[#1c1648] text-white px-4 py-2 cursor-pointer rounded-md hover:bg-indigo-600 mt-4 inline-block">
-            Click here to proceed to the next Lesson
+          Proceed to the Next Lesson
           </button>
         </Link>
       </div>
