2f2eb8f8fbcb823481dcc686c759f1828dd44075
Chibikate <queenie.cabanilla@gmail.com>
2024-03-11 19:30:13 +0800

    zoom in feature

--- components/3dslicer_page/tutorial_card.tsx
+++ components/3dslicer_page/tutorial_card.tsx
@@ -1,6 +1,6 @@
-import { StaticImageData } from "next/image";
-import React from "react";
+import { useState } from "react";
 import Image from "next/image";
+import { StaticImageData } from "next/image";
 
 interface Props {
   title: string;
@@ -10,19 +10,38 @@ interface Props {
 }
 
 const TutorialCard = ({ title, description, image, alt }: Props) => {
+  const [zoomed, setZoomed] = useState(false);
+
+  const handleZoom = () => {
+    setZoomed(!zoomed);
+  };
+
   return (
-    <div className="flex flex-col  justify-center flow-x-hidden ml-40 mr-40 text-black">
+    <div className="flex flex-col justify-center ml-40 mr-40 text-black">
       <div className="flex flex-col items-center justify-center dark:bg-white">
-        <p className="p-4 text-center font-bold text-md md:text-4xl rounded-lg text-[#130e30] ">
+        <p className="p-4 text-center font-bold text-md md:text-4xl rounded-lg text-[#130e30]">
           {title}
         </p>
         <p className="text-center flex-col sm:pt-14 p-7 sm:text-lg text-[#000000db]">
           {description}
         </p>
         {image !== null && image !== undefined && (
-          <Image src={image!} alt={alt} />
+          <div className="relative">
+            <button onClick={handleZoom} className="absolute top-0 right-0 z-10 p-2 bg-gray-200   text-black rounded-full hover:bg-[#8069ff] focus:outline-none">
+              +
+            </button>
+            <div className={`relative cursor-pointer ${zoomed ? 'zoomed' : ''}`} onClick={handleZoom}>
+              <Image src={image} alt={alt} className="cursor-pointer" />
+            </div>
+          </div>
         )}
       </div>
+      <style jsx>{`
+        .zoomed img {
+          transform: scale(2); /* Adjust the zoom level as needed */
+          transition: transform 0.3s ease-in-out;
+        }
+      `}</style>
     </div>
   );
 };
