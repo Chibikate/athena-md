5162be77c120669972a7bce077d4c7cc90a7cc79
Reynze <alturaszyrene3@gmail.com>
2024-07-16 23:33:04 +0800

    ✨ feat: Add new dicom lesson

---
+++ data/C2adding_data.tsx
@@ -0,0 +1,122 @@
+import * as C2 from "@/public/basic3D/C2";
+import Link from "next/link";
+
+export const slicerTutorial7 = [
+    {
+      title: "3D slicer - Adding DICOM datasets (Part 2)",
+      description: (
+        <div>
+          Make sure to <span className="font-bold">install</span> your downloaded{" "}
+          <span className="font-bold">3D slicer software</span> before you start
+          the lesson. 
+          <p>You can find it in the 3D interface (C1).</p>
+        </div>
+      ),
+      image: C2.picture2,
+      alt: "illustration",
+    },
+    {
+      title: "",
+      description:
+        (
+        <div>To <span className="font-bold">add datasets</span> to the program, left click on the  <span className="font-bold">add DICOM data button</span> located in the module interface panel.</div>
+        ),
+      image: C2.picture3,
+      alt: "illustration",
+    },
+    {
+      title: "",
+      description:
+        (
+          <div>
+            The <span className="font-bold">Add DICOM data button</span> is also available in the drop down menu in the menu bar under <span className="font-bold">“File”</span>.
+          </div>
+        ),
+      image: C2.picture4,
+      alt: "illustration",
+    },
+    {
+      title: "",
+      description:
+        (
+          <div><span className="font-bold">Left clicking</span> on the Add DICOM data will direct the user to the add DICOM database module.</div>
+        ),
+      image: C2.picture5,
+      alt: "illustration",
+    },
+    {
+      title: "",
+      description:
+        (
+          <div>The <span className="font-bold">DICOM data interface</span> will show the details of the available datasets.</div>
+        ),
+      image: C2.picture6,
+      alt: "illustration",
+    },
+    {
+      title: "",
+      description:
+        (
+          <div>The <span className="font-bold">search bar</span> above can be used to quickly search for previously imported datasets.</div>
+        ),
+      image: C2.picture7,
+      alt: "illustration",
+    },
+    {
+      title: "",
+      description:
+        (
+          <div>Once in the DICOM database module, <span className="font-bold">left click</span> on the <span className="font-bold">import DICOM files</span> button.</div>
+        ),
+      image: C2.picture8,
+      alt: "illustration",
+    },
+    {
+      title: "",
+      description:
+        (
+          <div>Navigate to your saved DICOM files and left click on the dataset of choice and click on <span className="font-bold">import</span>.</div>
+        ),
+      image: C2.picture9,
+      alt: "illustration",
+    },
+    {
+      title: "",
+      description:
+        (
+          <div>The selected dataset will be available in the DICOM database. <span className="font-bold">Left click</span> on the dataset and <span className="font-bold">left click</span> on the <span className="font-bold">load</span> button.</div>
+        ),
+      image: C2.picture10,
+      alt: "illustration",
+    },
+    {
+        title: "",
+        description:
+         (
+          <div>The DICOM images will immediately be loaded into the <span className="font-bold">DICOM display window</span> for viewing.</div>
+         ),
+        image: C2.picture11,
+        alt: "illustration",
+      },
+    {
+      title: "",
+      description: (
+        <div>
+          <p>
+            Congratulations! You have successfully added a DICOM dataset into 3D
+            slicer. For you to get the certificate, take the{" "}
+            <Link
+              href="/quiz/adddicom"
+              className="font-bold underline"
+            >
+              Quiz
+            </Link>{" "}
+            first.
+          </p>
+        </div>
+      ),
+      image: null,
+      alt: "",
+    },
+  ];
+  
