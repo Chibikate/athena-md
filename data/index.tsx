0e1e490fbe62402d2862c2489bd3b97569fe7c1d
Reynze <alturaszyrene3@gmail.com>
2024-07-16 23:36:26 +0800

    ✨ feat: Add new slicer tutorial

--- data/index.tsx
+++ data/index.tsx
@@ -4,6 +4,12 @@ import { slicerTutorial3 } from "./3dNavigating_data";
 import { slicerTutorial4 } from "./3dBasic_data";
 import { slicerTutorial5 } from "./3dAdvanced_data";
 import { MeshTutorial } from "./mesh_data";
+import { slicerTutorial6 } from "./C1interface_data";
+import { slicerTutorial7 } from "./C2adding_data";
+import { slicerTutorial8 } from "./C3navigating_data";
+import { slicerTutorial9 } from "./C4basic_data";
+import { slicerTutorial10 } from "./C5advanced_data";
+
 
 export {
   slicerTutorial1,
@@ -12,4 +18,9 @@ export {
   slicerTutorial4,
   slicerTutorial5,
   MeshTutorial,
+  slicerTutorial6,
+  slicerTutorial7,
+  slicerTutorial8,
+  slicerTutorial9,
+  slicerTutorial10,
 };
