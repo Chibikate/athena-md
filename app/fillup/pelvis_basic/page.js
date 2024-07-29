f810edffb895770a56d7ca88cd906cb68432b12a
Reynze <alturaszyrene3@gmail.com>
2024-07-29 12:06:39 +0800

    ✨ feat: Fillup for new lessons

--- app/fillup/pelvis_basic/page.js
+++ app/fillup/pelvis_basic/page.js
@@ -44,7 +44,7 @@ function FillUpPage() {
         <Link
           className="bg-indigo-500 text-white px-4 py-2 rounded-md hover:bg-indigo-600"
           href={{
-            pathname: "/certificate/meshmixer",
+            pathname: "/certificate/basicsegmentation",
             query: { name: fullName, date: date },
           }}
         >
