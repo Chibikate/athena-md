fe41d6c94e39b86551b4c74a700dd97cdd97910d
Reynze <alturaszyrene3@gmail.com>
2024-08-02 17:08:46 +0800

    🍱 assets: quiz1 pictures

---
+++ public/quiz1/index.tsx
@@ -0,0 +1,11 @@
+import Image1 from "@/public/quiz1/Image1.png";
+import Image2 from "@/public/quiz1/Image2.png";
+import Image3 from "@/public/quiz1/Image3.png";
+import Image4 from "@/public/quiz1/Image4.png";
+
+export {
+    Image1,
+    Image2,
+    Image3,
+    Image4,
+    };
