d707888da9880dcb2798094e5d309e2ac6822414
Reynze <alturaszyrene3@gmail.com>
2024-08-09 14:37:02 +0800

    ✨ feat: Edit cert interface

--- app/fillup/pelvis_basic/page.js
+++ app/fillup/pelvis_basic/page.js
@@ -44,7 +44,7 @@ function FillUpPage() {
         <Link
           className="bg-indigo-500 text-white px-4 py-2 rounded-md hover:bg-indigo-600"
           href={{
-            pathname: "/certificate/basicsegmentation",
+            pathname: "/certificate/pelvis_basic",
             query: { name: fullName, date: date },
           }}
         >
