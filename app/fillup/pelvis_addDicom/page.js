0a5cecc7ce291db0321d81c9ecfb89bc54c60bd3
Reynze <alturaszyrene3@gmail.com>
2024-08-13 11:01:48 +0800

    ✨ feat: Fillup for certificate updates

--- app/fillup/pelvis_addDicom/page.js
+++ app/fillup/pelvis_addDicom/page.js
@@ -44,7 +44,7 @@ function FillUpPage() {
         <Link
           className="bg-indigo-500 text-white px-4 py-2 rounded-md hover:bg-indigo-600"
           href={{
-            pathname: "/certificate/adddicom",
+            pathname: "/certificate/pelvis_addDicom",
             query: { name: fullName, date: date },
           }}
         >
--- app/fillup/pelvis_advanced/page.js
+++ app/fillup/pelvis_advanced/page.js
@@ -44,7 +44,7 @@ function FillUpPage() {
         <Link
           className="bg-indigo-500 text-white px-4 py-2 rounded-md hover:bg-indigo-600"
           href={{
-            pathname: "/certificate/advanced",
+            pathname: "/certificate/pelvis_advanced",
             query: { name: fullName, date: date },
           }}
         >
