ba77c550305310773fa41d81fb57b4c2f30814a1
Reynze <alturaszyrene3@gmail.com>
2024-08-17 22:02:51 +0800

    ✨ feat: 3D printing 4 lesson

---
+++ data/3dprinting4_data.tsx
@@ -0,0 +1,217 @@
+import * as E4 from "@/public/E4";
+import Link from "next/link";
+import Printing35 from "@/public/E4/35.png";
+import Printing36 from "@/public/E4/36.png";
+import Printing37 from "@/public/E4/37.png";
+import Printing38 from "@/public/E4/38.png";
+import Printing39 from "@/public/E4/39.png";
+import Printing40 from "@/public/E4/40.png";
+import Printing41 from "@/public/E4/41.png";
+import Printing42 from "@/public/E4/42.png";
+import Printing43 from "@/public/E4/43.png";
+import Printing44 from "@/public/E4/44.png";
+import Printing45 from "@/public/E4/45.png";
+import Printing46 from "@/public/E4/46.png";
+
+export const printingTutorial4 = [
+  {
+    title: "Introduction of Ultimaker Cura",
+    description: (
+      <div>
+        <div className="flex flex-col items-center justify-center">
+          <ul className=" space-y-2">
+            <li>
+              <span className="font-semibold pb-4">
+                Download Ultimaker Cura {" "}
+              </span>
+              <Link
+                href="  https://ultimaker.com/software/ultimaker-cura/"
+                className="underline"
+                target="_blank"
+              >
+                 https://ultimaker.com/software/ultimaker-cura/
+              </Link>
+              <p className="ml-6">Available in Windows, macOS and Linux </p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    ),
+    image:  Printing35,
+    alt: "",
+  },
+  {
+    title: "",
+    description: (
+      <div>
+        {" "}
+        <p>
+        Click the
+          <span className="font-bold text-lg">
+          “Download for free icon”
+          </span>{" "}
+           to get the latest version. Select the version according to the OS you are using.
+        </p>
+      </div>
+    ),
+    image: Printing36,
+    alt: "",
+  },
+  {
+    title: "",
+    description: (
+      <div>
+        <p>
+        Once download is done. You can proceed to install the application. 
+        Once you reach this part, this mean the software is ready to launch.
+        </p>
+      </div>
+    ),
+    image: Printing37,
+    alt: "",
+  },
+  {
+    title: "",
+    description: (
+      <div>
+        <p>
+        Congratulations on downloading the Ultimaker Cura software. On the next module, 
+        we will discuss the features of the software.{" "}         
+          <Link href="" className="font-bold underline">
+            Next Module
+          </Link>{" "}
+        </p>
+      </div>
+    ),
+    image: Printing38,
+    alt: "",
+  },
+  {
+    title: "",
+    description: (
+      <div>
+        <p>
+        Congratulations on downloading the Ultimaker Cura software. On the next module, 
+        we will discuss the features of the software.{" "}         
+          <Link href="" className="font-bold underline">
+            Next Module
+          </Link>{" "}
+        </p>
+      </div>
+    ),
+    image: Printing39,
+    alt: "",
+  },
+  {
+    title: "",
+    description: (
+      <div>
+        <p>
+        Congratulations on downloading the Ultimaker Cura software. On the next module, 
+        we will discuss the features of the software.{" "}         
+          <Link href="" className="font-bold underline">
+            Next Module
+          </Link>{" "}
+        </p>
+      </div>
+    ),
+    image: Printing40,
+    alt: "",
+  },
+  {
+    title: "",
+    description: (
+      <div>
+        <p>
+        Congratulations on downloading the Ultimaker Cura software. On the next module, 
+        we will discuss the features of the software.{" "}         
+          <Link href="" className="font-bold underline">
+            Next Module
+          </Link>{" "}
+        </p>
+      </div>
+    ),
+    image: Printing41,
+    alt: "",
+  },
+  {
+    title: "",
+    description: (
+      <div>
+        <p>
+        Congratulations on downloading the Ultimaker Cura software. On the next module, 
+        we will discuss the features of the software.{" "}         
+          <Link href="" className="font-bold underline">
+            Next Module
+          </Link>{" "}
+        </p>
+      </div>
+    ),
+    image: Printing42,
+    alt: "",
+  },
+  {
+    title: "",
+    description: (
+      <div>
+        <p>
+        Congratulations on downloading the Ultimaker Cura software. On the next module, 
+        we will discuss the features of the software.{" "}         
+          <Link href="" className="font-bold underline">
+            Next Module
+          </Link>{" "}
+        </p>
+      </div>
+    ),
+    image: Printing43,
+    alt: "",
+  },
+  {
+    title: "",
+    description: (
+      <div>
+        <p>
+        Congratulations on downloading the Ultimaker Cura software. On the next module, 
+        we will discuss the features of the software.{" "}         
+          <Link href="" className="font-bold underline">
+            Next Module
+          </Link>{" "}
+        </p>
+      </div>
+    ),
+    image: Printing44,
+    alt: "",
+  },
+  {
+    title: "",
+    description: (
+      <div>
+        <p>
+        Congratulations on downloading the Ultimaker Cura software. On the next module, 
+        we will discuss the features of the software.{" "}         
+          <Link href="" className="font-bold underline">
+            Next Module
+          </Link>{" "}
+        </p>
+      </div>
+    ),
+    image: Printing45,
+    alt: "",
+  },
+  {
+    title: "",
+    description: (
+      <div>
+        <p>
+        Congratulations on downloading the Ultimaker Cura software. On the next module, 
+        we will discuss the features of the software.{" "}         
+          <Link href="" className="font-bold underline">
+            Next Module
+          </Link>{" "}
+        </p>
+      </div>
+    ),
+    image: Printing46,
+    alt: "",
+  },
+];
