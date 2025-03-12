d6744a581d1318b41141e62e56636e848e2543e0
Reynze <alturaszyrene3@gmail.com>
2025-03-12 13:13:57 +0800

    ✨ feat: Add 3D printing image to main course assets

--- public/main_course/index.tsx
+++ public/main_course/index.tsx
@@ -1,7 +1,9 @@
 import Mandibular from "@/public/main_course/mandibular.png";
 import Pelvis from "@/public/main_course/pelvis.png";
+import Printing from "@/public/main_course/3d-printing.png";
 
 export {
   Mandibular,
   Pelvis,
+  Printing,
 };
