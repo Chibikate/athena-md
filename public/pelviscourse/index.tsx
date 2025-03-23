18b504c4641b9f87f296bc3ab7ea0908b2ab963c
Reynze <alturaszyrene3@gmail.com>
2025-03-23 11:16:47 +0800

    feat: add new images and update course module exports

---
+++ public/pelviscourse/index.tsx
@@ -0,0 +1,9 @@
+
+import Module8 from "@/public/pelviscourse/Module8.png";
+import Module9 from "@/public/pelviscourse/Module9.png";
+import Module10 from "@/public/pelviscourse/Module10.png";
+import Module11 from "@/public/pelviscourse/Module11.png";
+import Module12 from "@/public/pelviscourse/Module12.png";
+import Module13 from "@/public/pelviscourse/Module13.png";
+
+export { Module8, Module9, Module10, Module11, Module12, Module13};
---
+++ public/ultimakercourse/index.tsx
@@ -0,0 +1,7 @@
+
+import Module14 from "@/public/pelviscourse/Module10.png";
+import Module15 from "@/public/pelviscourse/Module11.png";
+import Module16 from "@/public/pelviscourse/Module12.png";
+import Module17 from "@/public/pelviscourse/Module13.png";
+
+export { Module14, Module15, Module16, Module17};
