137175fd870962a7e51ef91dde54fdf7447a2a2c
Reynze <alturaszyrene3@gmail.com>
2025-03-30 22:41:10 +0800

    feat: enhance certificate layouts for improved readability and consistency

--- components/certificate/basicsegmentation/basic_segmentation_certificate.jsx
+++ components/certificate/basicsegmentation/basic_segmentation_certificate.jsx
@@ -10,34 +10,57 @@ const interMonteCarlo = MonteCarlo({ subsets: ["latin"], weight: "400" });
 
 const BasicSegmentationCertificate = React.forwardRef(({ fullName, date }, ref) => {
   return (
-
-    <div className="container mx-auto flex flex-row items-center justify-center" ref={ref}>
-      <div className={`w-[845px] mx-auto  bg-[#ffffff] p-5 border-x-2 m-8 border-y-2 rounded-md shadow-md ${interGaramond.className} border-[#10172c] flex flex-col`}>
+    <div
+      className="container mx-auto flex flex-row items-center justify-center"
+      ref={ref}
+    >
+      <div
+        className={`w-[845px] mx-auto bg-[#ffffff] p-1 sm:p-5 border-x-2 m-8 border-y-2 rounded-md shadow-md 
+          ${interGaramond.className} border-[#10172c] flex flex-col relative`}
+      >
         <div className="rounded-full">
           <Image
             alt="Something never change"
             src={Image1}
             width={900}
-            height={283}
+            height={500}
+            className="w-full h-auto rounded-t-md"
           />
         </div>
 
-        <h2 className="absolute text-3xl font-semibold mb-4 pt-14 font-3xl ml-40 text-primary text-left">
-          CERTIFICATE <span className={`text-rose-600 italic ${interMonteCarlo.className}`}>Of</span> <br /> COMPLETION
-        </h2>
-        <p className="absolute pt-36 ml-40">This certificate is presented to</p>
-        <p className={`absolute ml-36 pt-48 text-3xl italic text-red-600 ${interMonteCarlo.className}`}>{fullName}</p>
+       {/* Certificate Heading */}
+       <div className="text-left pl-5 sm:pl-20 pt-10 absolute mt-0 p-20 pb-0 sm:mt-10 sm:text-center sm:pb-20">
+          <h2 className="text-sm font-semibold mb-0 sm:mb-4 text-primary sm:text-2xl md:text-3xl text-center">
+            CERTIFICATE{" "}
+            <span
+              className={`text-rose-600 italic text-center ${interMonteCarlo.className}`}
+            >
+              Of
+            </span>{" "}
+            <br /> COMPLETION
+          </h2>
 
-        <p className="absolute pt-64 ml-44">for successful completion of</p>
-        <p className="absolute pt-80 mb-24 ml-28 items-center"><span className="font-bold text-xl"J>BASIC SEGMENTATION</span><br/> in Web-based Tutorial System on the Standardized <br />
-          Process of Virtual Surgical Planning <br />
-          for Mandibular Reconstruction</p>
-        <p className="absolute mt-[450px] ml-[613px]">
-          {date}
-        </p>
+         {/* Recipient Info */}
+         <p className="text-sm sm:text-base md:text-lg text-center">This certificate is presented to</p>
+          <p
+            className={`text-md sm:text-3xl italic text-red-600 my-0 sm:my-4 text-center ${interMonteCarlo.className}`}
+          >
+            {fullName}
+          </p>
+
+          {/* Completion Details */}
+          <p className="text-sm sm:text-base md:text-lg text-center">for successful completion of</p>
+          <p className="font-bold text-lg sm:text-md md:text-lg my-0 sm:my-2 text-center ">3D Basic Segmentation</p>
+          < p className = "text-sm sm:text-base md:text-lg my-0 sm:my-1 text-center" >
+            in Web-based Tutorial System on the Standardized <br />
+            Process of Virtual Surgical Planning <br />
+            for Mandibular Reconstruction
+          </p>
+
+          <p className="absolute bottom-4 right-4 text-sm sm:text-base text-gray-700">{date}</p>
+        </div>
       </div>
     </div>
-   
   );
 });
 
--- components/certificate/meshmixer2/meshmixer2_certificate.jsx
+++ components/certificate/meshmixer2/meshmixer2_certificate.jsx
@@ -10,37 +10,59 @@ const interMonteCarlo = MonteCarlo({ subsets: ["latin"], weight: "400" });
 
 const Meshmixer2Certificate = React.forwardRef(({ fullName, date }, ref) => {
   return (
-
-    <div className="container mx-auto flex flex-row items-center justify-center" ref={ref}>
-      <div className={`w-[845px] mx-auto  bg-[#ffffff] p-5 border-x-2 m-8 border-y-2 rounded-md shadow-md ${interGaramond.className} border-[#10172c] flex flex-col`}>
+    <div
+      className="container mx-auto flex flex-row items-center justify-center"
+      ref={ref}
+    >
+      <div
+        className={`w-[845px] mx-auto bg-[#ffffff] p-1 sm:p-5 border-x-2 m-8 border-y-2 rounded-md shadow-md 
+          ${interGaramond.className} border-[#10172c] flex flex-col relative`}
+      >
         <div className="rounded-full">
           <Image
             alt="Something never change"
             src={Image1}
             width={900}
-            height={283}
+            height={500}
+            className="w-full h-auto rounded-t-md"
           />
         </div>
 
-        <h2 className="absolute text-3xl font-semibold mb-4 pt-14 font-3xl ml-40 text-primary text-left">
-          CERTIFICATE <span className={`text-rose-600 italic ${interMonteCarlo.className}`}>Of</span> <br /> COMPLETION
-        </h2>
-        <p className="absolute pt-36 ml-44">This certificate is presented to</p>
-        <p className={`absolute ml-36 pt-48 text-3xl italic text-red-600 ${interMonteCarlo.className}`}>{fullName}</p>
+        {/* Certificate Heading */}
+        <div className="text-left pl-5 sm:pl-20 pt-10 absolute mt-0 p-20 pb-0 sm:mt-10 sm:text-center sm:pb-20">
+          <h2 className="text-sm font-semibold mb-0 sm:mb-4 text-primary sm:text-2xl md:text-3xl text-center">
+            CERTIFICATE{" "}
+            <span
+              className={`text-rose-600 italic text-center ${interMonteCarlo.className}`}
+            >
+              Of
+            </span>{" "}
+            <br /> COMPLETION
+          </h2>
 
-        <p className="absolute pt-64 ml-44">for successful completion of</p>
-        <p className="absolute pt-80 mb-24 ml-28 items-center"><span className="font-bold text-xl"J>MESHMIXER VIRTUAL SURGERY </span><br/> in Mobile-Responsive Web-based Tutorial System <br />
-          on Virtual Surgical Planning Technologies <br />
-          for Pelvis Reconstruction</p>
-        <p className="absolute mt-[450px] ml-[613px]">
-          {date}
-        </p>
+          {/* Recipient Info */}
+          <p className="text-sm sm:text-base md:text-lg text-center">This certificate is presented to</p>
+          <p
+            className={`text-md sm:text-3xl italic text-red-600 my-0 sm:my-4 text-center ${interMonteCarlo.className}`}
+          >
+            {fullName}
+          </p>
+
+          {/* Completion Details */}
+          <p className="text-sm sm:text-base md:text-lg text-center">for successful completion of</p>
+          <p className="font-bold text-lg sm:text-md md:text-lg my-0 sm:my-2 text-center "> MeshMixer Virtual Surgery</p>
+          < p className = "text-sm sm:text-base md:text-lg my-0 sm:my-1 text-center" >
+            in Mobile-Responsve Tutorial System on <br />
+            Virtual Surgical Planning Technologies <br />
+            for Pelvis Reconstruction
+          </p>
+
+          <p className="absolute bottom-4 right-0 text-sm sm:text-base text-gray-700">{date}</p>
+        </div>
       </div>
     </div>
-   
   );
 });
-
 Meshmixer2Certificate.displayName = 'PMeshmixer2Certificate'; // Set the display name
 
 export default Meshmixer2Certificate;
