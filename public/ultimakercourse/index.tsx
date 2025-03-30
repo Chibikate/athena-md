869a66211d88da161c14faa74bec800da41ac7a3
Reynze <alturaszyrene3@gmail.com>
2025-03-30 21:14:50 +0800

    feat: update module imports to reflect new ultimakercourse structure

--- public/ultimakercourse/index.tsx
+++ public/ultimakercourse/index.tsx
@@ -1,7 +1,7 @@
 
-import Module14 from "@/public/pelviscourse/Module10.png";
-import Module15 from "@/public/pelviscourse/Module11.png";
-import Module16 from "@/public/pelviscourse/Module12.png";
-import Module17 from "@/public/pelviscourse/Module13.png";
+import Module14 from "@/public/ultimakercourse/Module14.png";
+import Module15 from "@/public/ultimakercourse/Module15.png";
+import Module16 from "@/public/ultimakercourse/Module16.png";
+import Module17 from "@/public/ultimakercourse/Module17.png";
 
 export { Module14, Module15, Module16, Module17};
