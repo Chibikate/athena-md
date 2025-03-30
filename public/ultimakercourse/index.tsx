e894876dbd5125fff6ce569c483a7c9d832f520e
Reynze <alturaszyrene3@gmail.com>
2025-03-31 07:39:51 +0800

    feat: add rotate GIF and update exports in ultimakercourse; update multiple images for consistency

--- public/ultimakercourse/index.tsx
+++ public/ultimakercourse/index.tsx
@@ -3,5 +3,6 @@ import Module14 from "@/public/ultimakercourse/Module14.png";
 import Module15 from "@/public/ultimakercourse/Module15.png";
 import Module16 from "@/public/ultimakercourse/Module16.png";
 import Module17 from "@/public/ultimakercourse/Module17.png";
+import Rotate from "@/public/ultimakercourse/rotate.gif";
 
-export { Module14, Module15, Module16, Module17};
+export { Module14, Module15, Module16, Module17, Rotate };
