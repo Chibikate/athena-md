47dc7e68cb67ccc444929790189c53e5ade2644d
Reynze <alturaszyrene3@gmail.com>
2025-04-26 21:41:13 +0800

    fix: enhance formatting and clarity in 3D Slicer tutorial descriptions

--- data/mandibular/3dinterface_data.tsx
+++ data/mandibular/3dinterface_data.tsx
@@ -10,19 +10,19 @@ export const slicerTutorial1 = [
     description: (
       <div>
         <div className="flex flex-col items-center justify-center">
-          <ul className=" space-y-2">
+          <ul className="space-y-2">
             <li>
               <span className="font-semibold pb-4">
-                Download 3D slicer (Stable version 5.6.1):{" "}
+                Download 3D Slicer (Stable version 5.6.1):{" "}
               </span>
               <Link
-                href=" https://download.slicer.org/"
+                href="https://download.slicer.org/"
                 className="underline"
                 target="_blank"
               >
                 https://download.slicer.org/
               </Link>
-              <p className="ml-6">Available in Windows, macOS and Linux </p>
+              <p className="ml-6">Available for Windows, macOS, and Linux</p>
             </li>
             <li>
               <span className="font-semibold">Download files: </span>
@@ -35,37 +35,35 @@ export const slicerTutorial1 = [
                   https://drive.google.com/drive/folders/1742qADzEkJDJ-FBMINb89dR9A7L0fbc6
                 </span>
               </Link>
-              <p className="ml-6">(in zipped folder contains dicom files)</p>
+              <p className="ml-6">(Zipped folder containing DICOM files)</p>
             </li>
           </ul>
         </div>
       </div>
     ),
-    image:  null,
+    image: null,
     alt: "",
   },
   {
     title: "",
     description: (
       <div>
-        {" "}
         <p>
           <span className="font-bold text-lg">
-            Virtual surgical planning (VSP)
+            Virtual Surgical Planning (VSP)
           </span>{" "}
-          is a planning technique that allows pre-surgical planning in a virtual
-          environment. This technology provides the surgeon with tools to
-          optimize the surgical plan in a risk-free environment. This plan can
-          be transferred into an actual physical template using 3D printing.
-          This module provides a step-by step process in the basic procedure of
-          utilizing a CT scan for virtual surgical planning of a mandibular
-          defect. This tutorial on VSP for mandibular reconstruction will serve
-          as a guide for individuals in the medical field, including both
-          students and professionals. Users will learn how to utilize VSP
-          software, such as 3D Slicer and MeshMixer, to create a digital model
-          of human anatomy, with a specific focus on mandibular reconstruction.
-          This tutorial also extends its applicability to individuals interested
-          in the innovative approach to medical.
+          is a technique that allows pre-surgical planning in a virtual
+          environment. This technology provides surgeons with tools to
+          optimize the surgical plan in a risk-free setting. The finalized plan can
+          be transferred into a physical template using 3D printing.
+          The following module provides a step-by-step guide for utilizing a CT scan
+          in virtual surgical planning of a mandibular defect. The VSP tutorial
+          serves as a guide for individuals in the medical field, including both
+          students and professionals. Users will learn how to utilize VSP software,
+          such as 3D Slicer and MeshMixer, to create a digital model of human
+          anatomy, with a specific focus on mandibular reconstruction.
+          The tutorial also applies to individuals interested in innovative approaches
+          to medical technology.
         </p>
       </div>
     ),
@@ -78,8 +76,15 @@ export const slicerTutorial1 = [
       <div>
         <p>
           In medical imaging,
-          <span className="font-bold"> Computed Tomography (CT) </span> is considered the gold standard for evaluating bone pathologies, lesions, defects, and other skeletal abnormalities due to its ability to produce detailed images of the inside of the body. CT scans are particularly effective in providing precise assessments of bone-related conditions. On the other hand,{" "}
-          <span className="font-bold"> Magnetic Resonance Imaging (MRI) </span>{" "} scans, while not ideal for visualizing bones, are valuable for generating detailed images of soft tissues and can contribute to creating a <span className="font-bold">three-dimensional (3D) </span>{" "} model of the human anatomy.
+          <span className="font-bold"> Computed Tomography (CT) </span> is considered
+          the gold standard for evaluating bone pathologies, lesions, defects,
+          and other skeletal abnormalities due to its ability to produce detailed images
+          of the inside of the body. CT scans are particularly effective in providing
+          precise assessments of bone-related conditions. On the other hand,{" "}
+          <span className="font-bold"> Magnetic Resonance Imaging (MRI) </span>{" "}
+          scans, while not ideal for visualizing bones, are valuable for generating
+          detailed images of soft tissues and can contribute to creating a{" "}
+          <span className="font-bold">three-dimensional (3D)</span> model of the human anatomy.
         </p>
       </div>
     ),
@@ -96,8 +101,7 @@ export const slicerTutorial1 = [
           </span>{" "}
           is the international standard for storing, transmitting, and managing
           medical imaging information and data. 3D printing in medicine uses
-          DICOM files to create custom anatomical parts, such as surgical
-          planning guides.
+          DICOM files to create custom anatomical parts, such as surgical planning guides.
         </p>
       </div>
     ),
@@ -109,7 +113,7 @@ export const slicerTutorial1 = [
     description: (
       <div>
         Make sure to <span className="font-bold">install</span> your downloaded{" "}
-        <span className="font-bold">3D slicer software</span> before you start
+        <span className="font-bold">3D Slicer software</span> before you start
         the lesson.
       </div>
     ),
@@ -120,8 +124,8 @@ export const slicerTutorial1 = [
     title: "",
     description: (
       <div>
-        The <span className="font-bold">menu bar </span> provides access to drop
-        down menu boxes.
+        The <span className="font-bold">menu bar</span> provides access to
+        drop-down menu boxes.
       </div>
     ),
     image: Module1.Slicer1,
@@ -142,9 +146,9 @@ export const slicerTutorial1 = [
     title: "",
     description: (
       <div>
-        This can be further customized by{" "}
-        <span className="font-bold">right clicking </span> on the toolbar and
-        <span className="font-bold">checking/unchecking </span> the preferred
+        The <span className="font-bold">toolbar</span> can be further customized
+        by <span className="font-bold">right-clicking</span> on it and{" "}
+        <span className="font-bold">checking/unchecking</span> the preferred
         choices.
       </div>
     ),
@@ -155,7 +159,7 @@ export const slicerTutorial1 = [
     title: "",
     description: (
       <div>
-        The <span className="font-bold">module interface </span> provides access
+        The <span className="font-bold">module interface</span> provides access
         to multiple options depending on the module loaded.
       </div>
     ),
@@ -166,7 +170,7 @@ export const slicerTutorial1 = [
     title: "",
     description: (
       <div>
-        Clicking on the <span className="font-bold">module drop down menu</span>{" "}
+        Clicking on the <span className="font-bold">module drop-down menu</span>{" "}
         will provide access to different modules available in the program.
       </div>
     ),
@@ -177,7 +181,7 @@ export const slicerTutorial1 = [
     title: "",
     description: (
       <div>
-        Alternatively, the user can search for a{" "}
+        Alternatively, users can search for a{" "}
         <span className="font-bold">module</span> by clicking on the{" "}
         <span className="font-bold">module finder icon.</span>
       </div>
@@ -202,7 +206,7 @@ export const slicerTutorial1 = [
     description: (
       <div>
         Different layouts for the DICOM display windows can be accessed by{" "}
-        <span className="font-bold">clicking on the drop down menu</span> in the
+        <span className="font-bold">clicking on the drop-down menu</span> in the
         menu bar.
       </div>
     ),
@@ -225,8 +229,8 @@ export const slicerTutorial1 = [
     description: (
       <div>
         <p>
-          Congratulations, you've to finish the lesson. For you to get the
-          certificate, take the{" "}
+          Congratulations, you have completed the lesson! To receive your
+          certificate, please take the{" "}
           <Link href="/quiz/interface" className="font-bold underline">
             Quiz
           </Link>{" "}
