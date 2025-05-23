62de6e51e3cca6619aa41a2efcd31c75f013a2cf
Reynze <alturaszyrene3@gmail.com>
2025-05-23 15:03:20 +0800

    refactor: update AboutSection layout and add new team member images for improved presentation

--- components/aboutus/about_section.tsx
+++ components/aboutus/about_section.tsx
@@ -1,44 +1,62 @@
 import React from "react";
 import kate from "@/public/kate.jpg";
 import lemuel from "@/public/Lemuel.jpg";
-import zyrene from "@/public/zyrene.jpg";
+import belle from "@/public/belle.jpg";
 import dianne from "@/public/dianne.jpg";
 import kaye from "@/public/kaye.jpg";
 import AboutCard from "@/components/aboutus/about_card";
 
 const AboutSection = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-10 place-items-center bg-white">
-      <AboutCard
-        image={lemuel}
-        alt="User Image"
-        name="Lemuel Velasco"
-        role=" Project Manager"
-      />
-      <AboutCard
-        image={kaye}
-        alt="User Image"
-        name="Kaye Velarde"
-        role="Pedagogy Manager"
-      />
-      <AboutCard
-        image={kate}
-        alt="User Image"
-        name="Kate Cabanilla"
-        role=" Web Developer"
-      />
-      <AboutCard
-        image={zyrene}
-        alt="User Image"
-        name="Zyrene Alturas"
-        role="Web Developer"
-      />
-      <AboutCard
-        image={dianne}
-        alt="User Image"
-        name="Dianne Arrubio"
-        role="Pedagogy Manager"
-      />
+    <div className="py-16 px-4 bg-white">
+      <div className="max-w-6xl mx-auto">
+
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-12">
+          {/* Leadership Row */}
+          <div className="sm:col-span-2 lg:col-span-1 flex justify-center">
+            <AboutCard
+              image={lemuel}
+              alt="Lemuel Velasco's photo"
+              name="Lemuel Velasco"
+              role="Project Manager"
+            />
+          </div>
+
+          <AboutCard
+            image={kaye}
+            alt="Kaye Velarde's photo"
+            name="Kaye Velarde"
+            role="Pedagogy Manager"
+          />
+
+          <AboutCard
+            image={dianne}
+            alt="Dianne Arrubio's photo"
+            name="Dianne Arrubio"
+            role="Pedagogy Manager"
+          />
+
+          {/* Development Team Row - Centered in their own row on larger screens */}
+          <div className="sm:col-span-1 lg:col-start-1 lg:col-end-2 flex justify-center">
+            <AboutCard
+              image={kate}
+              alt="Kate Cabanilla's photo"
+              name="Kate Cabanilla"
+              role="Web Developer"
+            />
+          </div>
+
+          <div className="sm:col-span-1 lg:col-start-2 lg:col-end-3 flex justify-center">
+            <AboutCard
+              image={belle}
+              alt="Zyrene Alturas's photo"
+              name="Zyrene Alturas"
+              role="Web Developer"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
